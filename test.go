3f6ea52ca2ed6826acd8aceec7665d10099bed7e
joexzh <xiezhenhao@gmail.com>
2022-02-22 16:41:33 +0800

    include test.go

---
+++ test.go
@@ -0,0 +1,32 @@
+package main
+
+func test() {
+	// ctx := context.Background()
+	// repo, err := repos.NewConceptRepo(ctx)
+	// if err != nil {
+	//	log.Fatal(err)
+	// }
+	// concept, err := repo.QueryByConceptId(ctx, "301558")
+	// if err != nil {
+	//	log.Fatal(err)
+	// }
+	// //concept.Define = "1" + concept.Define
+	// //
+	// //updated, err := repo.Update(ctx, *concept)
+	// //if err != nil {
+	// //	log.Fatal(err)
+	// //}
+	// //fmt.Println("updated", updated)
+	//
+	// deleted, updated, err := repo.UpdateStockConcept(ctx, *concept)
+	// if err != nil {
+	//	log.Fatal(err)
+	// }
+	// fmt.Println("deleted", deleted, "updated", updated)
+
+	// file, _ := os.Executable()
+	// file = path.Clean(file)
+	// dir := path.Dir(file)
+	// fmt.Println(dir)
+
+}
